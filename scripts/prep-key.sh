f1fc7659f18e5b21030b97ad8eb03641bd163053
iamareebjamal <jamal.areeb@gmail.com>
2017-08-26 16:55:36 +0530

    add encrypted tar

--- scripts/prep-key.sh
+++ scripts/prep-key.sh
@@ -8,5 +8,5 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "iamareebjamal/
     exit 0
 fi
 
-openssl aes-256-cbc -K $encrypted_4dd7e7e22f80_key -iv $encrypted_4dd7e7e22f80_iv -in ./scripts/key.jks.enc -out ./scripts/key.jks -d
-openssl aes-256-cbc -K $encrypted_4dd7e7e22f80_key -iv $encrypted_4dd7e7e22f80_iv -in ./scripts/fastlane.json.enc -out ./scripts/fastlane.json -d
+openssl aes-256-cbc -K $encrypted_4dd7e7e22f80_key -iv $encrypted_4dd7e7e22f80_iv -in ./scripts/secrets.tar.enc -out ./scripts/secrets.tar -d
+tar xvf secrets.tar
