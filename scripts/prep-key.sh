f6873f34df55323efbc14338ed00de269e913d59
iamareebjamal <jamal.areeb@gmail.com>
2017-08-26 17:02:15 +0530

    fix tar command

--- scripts/prep-key.sh
+++ scripts/prep-key.sh
@@ -9,4 +9,4 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "iamareebjamal/
 fi
 
 openssl aes-256-cbc -K $encrypted_4dd7e7e22f80_key -iv $encrypted_4dd7e7e22f80_iv -in ./scripts/secrets.tar.enc -out ./scripts/secrets.tar -d
-tar xvf secrets.tar
+tar xvf ./scripts/secrets.tar -C scripts/
