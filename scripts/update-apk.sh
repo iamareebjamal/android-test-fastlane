17e12d85d4cf4824cb6680010cc4ccc6389855a9
iamareebjamal <jamal.areeb@gmail.com>
2017-09-06 22:58:47 +0530

    chore: Split in dev and master

--- scripts/update-apk.sh
+++ scripts/update-apk.sh
@@ -4,21 +4,25 @@ set -e
 git config --global user.name "Travis CI"
 git config --global user.email "noreply+travis@iamareebjamal.org"
 
-export DEPLOY_BRANCH=${DEPLOY_BRANCH:-master}
+export DEPLOY_BRANCH=${DEPLOY_BRANCH:-development}
+export PUBLISH_BRANCH=${PUBLISH_BRANCH:-master}
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "iamareebjamal/android-test-fastlane" -o  "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
-    echo "We upload apk only for changes in master. So, let's skip this shall we ? :)"
+    echo "We upload apk only for changes in development, and not PRs. So, let's skip this shall we ? :)"
     exit 0
 fi
-
 
 git clone --quiet --branch=apk https://iamareebjamal:$GITHUB_API_KEY@github.com/iamareebjamal/android-test-fastlane apk > /dev/null
 cd apk
 rm *.apk
 cp ../app/build/outputs/apk/*.apk .
-cp app-release-unsigned.apk app-release-unaligned.apk
-jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../scripts/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-release-unaligned.apk $ALIAS
-${ANDROID_HOME}/build-tools/25.0.2/zipalign -v -p 4 app-release-unaligned.apk app-release.apk
+
+if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
+    echo "Push to master branch detected, signing the app..."
+    cp app-release-unsigned.apk app-release-unaligned.apk
+	jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../scripts/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-release-unaligned.apk $ALIAS
+	${ANDROID_HOME}/build-tools/25.0.2/zipalign -v -p 4 app-release-unaligned.apk app-release.apk
+fi
 
 for file in *; do
   mv $file test-${file%%}
@@ -40,6 +44,10 @@ git branch -m apk
 git push origin apk --force --quiet > /dev/null
 
 # Publish App to Play Store
+if [ "$TRAVIS_BRANCH" != "$PUBLISH_BRANCH" ]; then
+    echo "We publish apk only for changes in master branch. So, let's skip this shall we ? :)"
+    exit 0
+fi
 
 gem install fastlane
 fastlane supply --apk test-app-release.apk --track alpha --json_key ../scripts/fastlane.json --package_name com.iamareebjamal.fastlane
