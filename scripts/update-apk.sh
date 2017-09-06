454f21fe7717778f6b008bd8fabf853ef4de6c8f
iamareebjamal <jamal.areeb@gmail.com>
2017-09-06 23:40:20 +0530

    chore: Change in permitted branches

--- scripts/update-apk.sh
+++ scripts/update-apk.sh
@@ -7,8 +7,14 @@ git config --global user.email "noreply+travis@iamareebjamal.org"
 export DEPLOY_BRANCH=${DEPLOY_BRANCH:-development}
 export PUBLISH_BRANCH=${PUBLISH_BRANCH:-master}
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "iamareebjamal/android-test-fastlane" -o  "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
-    echo "We upload apk only for changes in development, and not PRs. So, let's skip this shall we ? :)"
+echo $TRAVIS_BRANCH
+echo $DEPLOY_BRANCH
+echo $PUBLISH_BRANCH
+echo $TRAVIS_PULL_REQUEST
+echo $TRAVIS_REPO_SLUG
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "iamareebjamal/android-test-fastlane" ] || ! [ "$TRAVIS_BRANCH" == "$DEPLOY_BRANCH" -o "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
+    echo "We upload apk only for changes in development or master, and not PRs. So, let's skip this shall we ? :)"
     exit 0
 fi
 
